c88edd068750172086f89cf9b54cb91543e54892
sterling#elysian.JP60=={56} <shohan.sub.56@gmail.com>
2022-08-12 00:26:07 +0600

    class 1

---
+++ script.js
@@ -0,0 +1,99 @@
+console.log("Hello, World!");
+console.log("Ajke amar mon valo ase!");
+
+// variables
+// let name = "Shohan";
+let number = 20;
+let temperature = 38;
+let device = "Desktop";
+
+console.log(temperature);
+
+// variable naming convention (camel case)
+let herName = "Nishi Konna";
+let myBikeBrand = "Suzuki";
+let firstName = "Tanisha";
+let lastName = "Tanvin";
+let ramFrequencyInTimeOverTorqueSpeed = 3200;
+let herBikeName = "Kawasaki";
+let boy_captain = "Sadril";
+let girl_captain = "Rekha";
+let YOUR_MONITOR_PRICE = 15000;
+let $unknown_girl_name = "unknown";
+let PI = 3.1416;
+
+// let 10mafia_name = 'daku';
+// let bf&gf = 'unknown';
+// let new-number = 100;
+// let new = 10;
+// let true = 10;
+// let false = 20;
+// let function = 10;
+// let let = 30;
+// let Person = "Puzon";
+// let bike1 = "Suzuki";
+// let bike2 = "Kawasaki";
+
+console.log(firstName);
+// console.log(name);
+
+/*
+ data types: object or primitive
+ primitive data types:
+ 1. number (integer, floating)
+ 2. string
+ 3. boolean
+ 4. undefined
+ 5. null
+ 6. symbol
+ 7. bigInt
+
+ value has type, not variable
+*/
+
+let movieRating = 7.8;
+let movieName = "inception";
+let isRaining = false;
+let isSheCute = true;
+let isMarried = false;
+let isJavaScriptFun;
+
+console.log(typeof movieRating);
+console.log(typeof movieName);
+console.log(typeof isRaining);
+console.log(typeof isSheCute);
+console.log(typeof isJavaScriptFun);
+
+// re-assign or mutate
+isJavaScriptFun = true;
+console.log(typeof isJavaScriptFun);
+
+// re-assign
+isJavaScriptFun = 80;
+console.log(typeof isJavaScriptFun);
+
+let carSpeed = null;
+console.log(typeof carSpeed);
+
+// null isn't an object, it's a bug
+
+let age = 30; // declareing a variable using let keyword
+age = 31; // re-assign or mutate
+age = 50;
+
+const birthYear = 1720; // decaring a variable using const keyword
+/* birthYear = 1800; // re-assign not possible in a constant variable */
+
+// never use var
+var planMasterName = "Mr. X";
+planMasterName = "Mr. Y";
+
+// never use without keyword
+fatFriendName = "Janina";
+console.log(fatFriendName);
+
+const myLocation = "Dhaka";
+const distance = 235;
+console.log(myLocation);
+console.log(distance);
+console.log(typeof distance);
